3aff6a32a0739be7344e8cde4620e3834faeb5f0
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-11-30 13:09:49 +0530

    Pass children props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Alert>
+        <span>Hello world</span>
+      </Alert>
     </div>
   );
 }
